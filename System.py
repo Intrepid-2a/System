f0e9f49649f906b897674e6f3d3d3df19117458a
Marius <thartbm@gmail.com>
2024-02-20 13:36:56 -0500

    add fusion stimuli to system setup

--- System.py
+++ System.py
@@ -99,6 +99,68 @@ def localizeSetup( trackEyes, filefolder, location=None, glasses='RG' ):
               'red_col'  : red_col,
               'blue_col' : blue_col }
 
+    fusion = {'hi': fusionStim(win = win,
+                               pos = [0,5]),
+              'lo': fusionStim(win = win,
+                               pos = [0,5])}
+
     return({'win'     : win,
             'tracker' : ET,
-            'colors'  : colors})
+            'colors'  : colors,
+            'fusion'  : fusion})
+
+
+
+from psychopy import visual
+import random
+import numpy as np
+
+
+class fusionStim:
+
+    def __init__(self, 
+                 win, 
+                 pos     = [0,0],
+                 colors  = [[-1,-1,-1],[1,1,1]],
+                 rows    = 15,
+                 columns = 7,
+                 square  = 0.5,
+                 units   = 'deg'):
+
+        self.win     = win
+        self.pos     = pos
+        self.colors  = colors
+        self.rows    = rows
+        self.columns = columns
+        self.square  = square
+        self.units   = units
+
+        self.resetProperties()
+
+    def resetProperties(self):
+        self.nElements = (self.columns*2 + 1) * self.rows
+
+        self.setColorArray()
+        self.setPositions()
+        self.createElementArray()
+
+    def setColorArray(self):
+        self.colorArray = (self.colors * int(np.ceil(((self.columns*2 + 1) * self.rows)/len(self.colors))))
+        random.shuffle(self.colorArray)
+        self.colorArray = self.colorArray[:self.nElements]
+
+    def setPositions(self):
+        self.xys = [[(i * self.square)+self.pos[0], ((j-((self.rows-1)/2))*self.square)+self.pos[1]] for i in range(-self.columns, self.columns + 1) for j in range(self.rows)]
+
+    def createElementArray(self):
+        self.elementArray = visual.ElementArrayStim( win         = self.win, 
+                                                     nElements   = self.nElements,
+                                                     sizes       = self.square, 
+                                                     xys         = self.xys, 
+                                                     colors      = self.colorArray, 
+                                                     units       = self.units, 
+                                                     elementMask = 'none', 
+                                                     sfs         = 0)
+
+    def draw(self):
+        self.elementArray.draw()
